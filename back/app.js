9681c074387a508d45045103f4b91d0f32ccd043
HEXACTA\iguevara <iguevara@hexacta.com>
2019-05-20 01:20:34 -0300

    adding cors framework to test

--- back/app.js
+++ back/app.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const swapi = require('swapi-node')
 const app = express()
+const cors = require('cors')
 const port = 3000
+
+app.use(cors())
 
 const func = async function(req, res) {
   let planets = await swapi.get('https://swapi.co/api/planets/').then(function(planets) {
@@ -9,6 +12,6 @@ const func = async function(req, res) {
   })
   res.json(planets)
 }
-app.get('/', func)
+app.get('/', cors(), func)
 
 app.listen(port, () => console.log(`Listening in port ${port}`))
